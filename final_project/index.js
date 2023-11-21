662a5add5f2bc7d79ac9c478e57ebd99f86140b9
0xOrigin <egyahmed.ezzat120@gmail.com>
2023-11-21 03:34:07 +0200

    Add session authentication middleware

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,26 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({secret:"fingerprint_customer", resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next){
+    let token = req.session.authorization;
+    if(!token)
+        return res.status(401).json({message: "User is not logged in!"})
+
+    token = token['accessToken'];
+    jwt.verify(token, "access", (err, user) => {
+        if(err)
+            return res.status(401).json({message: "User is not authenticated!"})
+        
+        req.user = user;
+        next();
+    });
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
